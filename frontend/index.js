90e385f52893481a8d25d5e7d4890d3619151416
AstridHofferson <miria.costapereira@gmail.com>
2020-06-22 15:37:31 -0300

    frontend

---
+++ frontend/index.js
@@ -0,0 +1,12 @@
+var msg = 'Mensagem marota';
+
+function alerta(mensagem){
+    alert(msg);
+}
+
+function metade(num, por){
+    return num / por;
+}
+
+// alerta('A metade de 15 é: ' + metade(15, 2));
+alerta();
