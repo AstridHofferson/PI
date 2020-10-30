42aaca5c378d837a44ceb2351f46921952897921
AstridHofferson <miria.costapereira@gmail.com>
2020-10-30 17:36:12 -0300

    backend

--- frontend/lerViver/lerViver.js
+++ frontend/lerViver/lerViver.js
@@ -1,6 +1,6 @@
-async function handleShelf() {
-  response = await fetch('http://localhost:3333/books');
-  books = await response.json();
+async function handleShelf() { //função assíncrona não lê a próxima linha até a linha atual ter executado
+  response = await fetch('http://localhost:3333/books'); //pegando informações da tabela livros
+  books = await response.json(); //transformando arquivo json em objeto js
   
   response = await fetch('http://localhost:3333/covers');
   covers = await response.json();
@@ -10,25 +10,25 @@ async function handleShelf() {
   const randomizedBooks = [];
 
   for (let i = 0; i < 8; i++) {
-    book = books[Math.floor(Math.random() * books.length)];
+    book = books[Math.floor(Math.random() * books.length)]; //(arredonda num pra baixo(cria num aleatorio de 0 a 1))
     
-    if (!randomizedBooks.includes(book)) {
-      randomizedBooks.push(book);
+    if (!randomizedBooks.includes(book)) { //testa se não tem o livro no array
+      randomizedBooks.push(book); //colocar um livro no array
     } else {
       i--;
     }
   };
   
-  randomizedBooks.forEach((book, index) => {
+  randomizedBooks.forEach((book, index) => { //a function vai acontecer para cada livro que tiver no array
 
-    const cover = covers.find(cover => cover.id === book.id);
+    const cover = covers.find(cover => cover.id === book.id); //pegar as capas do cover que tenham o mesmo id do livro
 
-    const divCapaTitulo = document.createElement('div');
-    divCapaTitulo.setAttribute('class', 'capa-titulo');
+    const divCapaTitulo = document.createElement('div'); //criando uma div
+    divCapaTitulo.setAttribute('class', 'capa-titulo'); //colocando atributos (atributo que vai ser colocado, valor do atributo)
     divCapaTitulo.setAttribute('id', book.id);
 
     divCapaTitulo.addEventListener('click', () => {
-      window.location.href = `http://127.0.0.1:5501/frontend/pgLivro/index.html?idLivro=${book.id}`;
+      window.location.href = `http://127.0.0.1:5501/frontend/pgLivro/index.html?idLivro=${book.id}`; //quando clicar no livro entra no link
     })
 
     const imgCapa = document.createElement('img');
@@ -37,13 +37,30 @@ async function handleShelf() {
 
     const divText = document.createElement('div');
     divText.setAttribute('class', 'text');
-    divText.innerHTML = book.title;
+    divText.innerHTML = book.title; //adicionando texto na div
 
-    divCapaTitulo.appendChild(imgCapa);
+    divCapaTitulo.appendChild(imgCapa); //colocando imgCapa na div
     divCapaTitulo.appendChild(divText);
     
     shelves[0].appendChild(divCapaTitulo);
   });
+
+  // //categoria dos livros recém-publicados
+  // const latestBooks = [];
+
+  // const releaseDates = books.map(book => {
+  //   const releaseDateSize = book.publicationDate.length;
+  //   const year = book.publicationDate.slice(releaseDateSize - 4);
+  //   return year;
+  // });
+
+  // releaseDates.sort(); //arruma a ordem dos anos de mais velho pra mais novo
+  // releaseDates.reverse(); //coloca na ordem de mais novo pra mais velho
+  // console.log(releaseDates);
+
+  // for (let i = 0; i < 8; i++) {
+  //   latestBooks.push(books.find(book => ));
+  // };
 }
 
 handleShelf();
--- frontend/pgLivro/pgLivro.js
+++ frontend/pgLivro/pgLivro.js
@@ -3,7 +3,7 @@ const info = document.getElementsByClassName('informacoes');
 const title = document.getElementsByClassName('titulo');
 const imgCover = document.getElementsByClassName('capa');
 
-const urlParams = new URLSearchParams(location.search);
+const urlParams = new URLSearchParams(location.search); //pegando parametros que vem pela URL
 
 async function handleBook() {
   response = await fetch(`http://localhost:3333/books/${urlParams.get('idLivro')}`);
@@ -14,7 +14,7 @@ async function handleBook() {
 
   const cover = covers.find(cover => cover.id === book.id);
 
-  title[0].innerHTML = book.title;
+  title[0].innerHTML = book.title; //colocando o título do livro na pg
 
   imgCover[1].setAttribute('src', cover.image_url);
 
@@ -26,7 +26,7 @@ async function handleBook() {
   info[5].innerHTML = `<b>Editora(s):</b> ${book.publisher}`;
   info[6].innerHTML = `<b>Preço:</b> ${book.price}`;
 
-  synopsis[0].children[1].innerHTML = book.synopsis
+  synopsis[0].children[1].innerHTML = book.synopsis //colocando texto dentro do segundo filho de sinopse
 }
 
 handleBook();
