43487a4c3fc4a9fe4b498d6fb7f35a00e83440db
AstridHofferson <miria.costapereira@gmail.com>
2020-11-08 22:05:25 -0300

    backend

--- frontend/lerViver/lerViver.js
+++ frontend/lerViver/lerViver.js
@@ -45,22 +45,47 @@ async function handleShelf() { //função assíncrona não lê a próxima linha 
     shelves[0].appendChild(divCapaTitulo);
   });
 
-  // //categoria dos livros recém-publicados
-  // const latestBooks = [];
+  //categoria dos livros recém-publicados
+  const latestBooks = [];
 
-  // const releaseDates = books.map(book => {
-  //   const releaseDateSize = book.publicationDate.length;
-  //   const year = book.publicationDate.slice(releaseDateSize - 4);
-  //   return year;
-  // });
+  const releaseDates = books.map(book => { //passando por todos os objetos do array de livros /map retorna array
+    const releaseDateSize = book.publicationDate.length; //pegando tamanho da string
+    const year = book.publicationDate.slice(releaseDateSize - 4); //selecionando 4 caracteres antes de terminar a string
+    return year;
+  });
 
-  // releaseDates.sort(); //arruma a ordem dos anos de mais velho pra mais novo
-  // releaseDates.reverse(); //coloca na ordem de mais novo pra mais velho
-  // console.log(releaseDates);
+  releaseDates.sort(); //arruma a ordem dos anos de mais velho pra mais novo
+  releaseDates.reverse(); //coloca na ordem de mais novo pra mais velho
 
-  // for (let i = 0; i < 8; i++) {
-  //   latestBooks.push(books.find(book => ));
-  // };
+  for (let i = 0; i < 8; i++) {
+    latestBooks.push(books.find(book => book.publicationDate.includes(releaseDates[i]))); //testa se publicationDate tem o valor de releaseDates[i]
+  };
+
+  latestBooks.forEach((book, index) => { 
+
+    const cover = covers.find(cover => cover.id === book.id);
+
+    const divCapaTitulo = document.createElement('div');
+    divCapaTitulo.setAttribute('class', 'capa-titulo');
+    divCapaTitulo.setAttribute('id', book.id);
+
+    divCapaTitulo.addEventListener('click', () => {
+      window.location.href = `http://127.0.0.1:5501/frontend/pgLivro/index.html?idLivro=${book.id}`;
+    })
+
+    const imgCapa = document.createElement('img');
+    imgCapa.setAttribute('class', 'capa');
+    imgCapa.setAttribute('src', cover.image_url);
+
+    const divText = document.createElement('div');
+    divText.setAttribute('class', 'text');
+    divText.innerHTML = book.title;
+
+    divCapaTitulo.appendChild(imgCapa);
+    divCapaTitulo.appendChild(divText);
+    
+    shelves[2].appendChild(divCapaTitulo);
+  });
 }
 
 handleShelf();
